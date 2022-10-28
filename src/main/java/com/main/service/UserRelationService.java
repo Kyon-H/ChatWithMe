5c96f6a6777ae7863981f6bc21abbbb585a9f8d7
kyon <69248710+Kyon-H@users.noreply.github.com>
2022-10-28 19:44:11 +0800

    Update UserRelationService.java

--- src/main/java/com/main/service/UserRelationService.java
+++ src/main/java/com/main/service/UserRelationService.java
@@ -5,15 +5,15 @@ import java.util.List;
 import com.main.entity.UserRelation;
 
 public interface UserRelationService {
-		//���id��ȡ�û�
+		
 		public UserRelation getUserRelation(int idA, int idB);
-		//����û�
+		
 		public void addUserRelation(UserRelation userRelation);
-		//ɾ���û�
+		
 		public boolean deleteUserRelation(int idA, int idB);
-		//�����û�
+		
 		public boolean updateUser(UserRelation userRelation);
-		//��ȡ�����û�
+		
 		@SuppressWarnings("rawtypes")
 		public List getAllFriends(int id);
 }
